70e2a772b26e4ea9e94ea514f9ccd1cfed015612
fatih kahraman <fatih.kahraman@comeon.com>
2023-04-04 00:45:17 +0100

    Use botkit to create and initialize bot

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const { Botkit } = require('botkit')
+const { SlackAdapter, SlackEventMiddleware } = require(
+  'botbuilder-adapter-slack')
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+/**
+ * Returns the secret string from Google Cloud Secret Manager
+ * @param {string} name The name of the secret.
+ * @return {string} The string value of the secret.
+ */
+async function accessSecretVersion (name) {
+  const client = new SecretManagerServiceClient()
+  const projectId = process.env.PROJECT_ID
+  const [version] = await client.accessSecretVersion({
+    name: `projects/${projectId}/secrets/${name}/versions/1`
+  })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  // Extract the payload as a string.
+  const payload = version.payload.data.toString('utf8')
+
+  return payload
+}
+
+/**
+ * Asynchronous function to initialize bot.
+ */
+async function botInit () {
+  const adapter = new SlackAdapter({
+    clientSigningSecret: await accessSecretVersion('client-signing-secret'),
+    botToken: await accessSecretVersion('bot-token')
+  })
+
+  adapter.use(new SlackEventMiddleware())
+
+  const controller = new Botkit({
+    webhook_uri: '/vodka',
+    adapter: adapter
+  })
+
+  controller.ready(() => {
+    controller.on('slash_command', async(bot, message) => {
+      const params = message.text.split[' '];
+
+      bot.replyPublic(`You typed ${message}`);
+    })
+  })
+}
+
+botInit()
